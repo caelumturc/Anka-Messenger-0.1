cdee426158b2821a70e9b8730ffc178af743bb22
caelumturc <87136151+caelumturc@users.noreply.github.com>
2021-09-10 07:30:33 -0700

    Add files via upload

---
+++ Anka messenger/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Anka messenger
+//
+//  Created by goktan on 2.09.2021.
+//  Copyright © 2021 caelum. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
